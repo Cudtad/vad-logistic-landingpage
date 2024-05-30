93f3fee2c0b0d57ce9c75fd00f81382c24024714
DatND <datnd@nami.trade>
2024-05-30 09:10:12 +0700

    update

--- src/components/common/collapsible/index.tsx
+++ src/components/common/collapsible/index.tsx
@@ -1,6 +1,7 @@
+import React, { useState, useRef, useEffect } from "react";
 import { TriangleDownIcon } from "@radix-ui/react-icons";
 import clsx from "clsx";
-import React, { useState } from "react";
+import "./style.scss"; // Import the custom CSS file
 
 interface CollapsibleProps {
   title: string;
@@ -9,10 +10,19 @@ interface CollapsibleProps {
 
 const Collapsible: React.FC<CollapsibleProps> = ({ title, children }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const contentRef = useRef<HTMLDivElement>(null);
 
   const toggleOpen = () => {
     setIsOpen(!isOpen);
   };
+
+  useEffect(() => {
+    if (contentRef.current) {
+      contentRef.current.style.maxHeight = isOpen
+        ? `${contentRef.current.scrollHeight}px`
+        : "0px";
+    }
+  }, [isOpen]);
 
   return (
     <div>
@@ -21,12 +31,11 @@ const Collapsible: React.FC<CollapsibleProps> = ({ title, children }) => {
         className="flex justify-between items-center w-full"
       >
         <span>{title}</span>
-        <TriangleDownIcon className={clsx({"rotate-180": isOpen})} />
+        <TriangleDownIcon className={clsx({ "rotate-180": isOpen })} />
       </button>
       <div
-        className={`transition-all duration-300 ease-in-out ${
-          isOpen ? "max-h-screen" : "max-h-0 overflow-hidden"
-        }`}
+        ref={contentRef}
+        className={clsx("collapsible-content", { open: isOpen })}
       >
         <div className="p-4 flex flex-col gap-y-2">{children}</div>
       </div>
